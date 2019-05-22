c3e836058becd2e7b84b6b9a374b9c4078e460d6
polarnik <owasp@yandex.ru>
2019-05-22 10:35:38 +0300

    jmeter-plugins SharedMap

---
+++ src/main/java/SharedHashMap.java
@@ -0,0 +1,15 @@
+import java.util.concurrent.ConcurrentHashMap;
+
+public class SharedHashMap
+{
+    private static final ConcurrentHashMap instance = new ConcurrentHashMap();
+
+    public static ConcurrentHashMap GetInstance()
+    {
+        return instance;
+    }
+
+    public String test() {
+        return "SharedHashMap";
+    }
+}
